5ec9de90064fc170040c123566c25718b66a6076
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-12 14:27:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,26 @@ module.exports = {
 
 botName: "TOKI RP BOT V2",
 
+// Gemini API key dari Railway
 geminikey: process.env.GEMINI_API_KEY,
 
+// AI model
 ai: {
 model: "gemini-1.5-flash"
 },
 
+// typing simulation speed
+typingSpeed: {
+min: 800,
+max: 4000
+},
+
+// memory conversation
 memoryLimit: 12,
 
+// chance system
 sceneChance: 0.25,
-
 dreamChance: 0.05,
-
 voiceChance: 0.2
 
 }
